08176ab52a47771ccf91efe2833ec1a4b7d8edf5
Adolfo <adolfopare9@gmail.com>
2026-01-30 14:36:56 -0500

    Introduccion a JSON

---
+++ 13.JSON/estructura_razas.py
@@ -0,0 +1,90 @@
+# Razas de personajes para videojuego
+
+razas = {
+    "Humanos": [
+        {
+            "Guerrero": {
+                "Vida": 150,
+                "ID": 1,
+                "Ataque": 70,
+                "Defensa": 35,
+                "Poder mágico": 0,
+
+            }
+        },
+        {
+            "Jinete": {
+                "Vida": 190,
+                "ID": 2,
+                "Ataque": 40,
+                "Defensa": 40,
+                "Poder mágico": 0,
+
+            }
+        },
+        {
+            "Mago": {
+                "Vida": 100,
+                "ID": 3,
+                "Ataque": 60,
+                "Defensa": 20,
+                "Poder mágico": 100,
+
+            }
+        },
+        {
+            "Recolector": {
+                "Vida": 130,
+                "ID": 4,
+                "Ataque": 50,
+                "Defensa": 60,
+                "Poder mágico": 50,
+
+            }
+        },  
+        {
+            "Observador": {
+                "Vida": 150,
+                "ID": 5,
+                "Ataque": 0,
+                "Defensa": 70,
+                "Poder mágico": 60,
+
+            }
+        }, 
+    ],
+    "Orcos": [
+        {
+            "Guerrero": {
+                "Vida": 200,
+                "ID": 6,
+                "Ataque": 40,
+                "Defensa": 20,
+                "Poder mágico": 0,
+
+            }
+        },
+        {
+            "Chamán": {
+                "Vida": 70,
+                "ID": 7,
+                "Ataque": 40,
+                "Defensa": 10,
+                "Poder mágico": 150,
+
+            }
+        },
+        {
+            "Jinete": {
+                "Vida": 220,
+                "ID": 8,
+                "Ataque": 25,
+                "Defensa": 25,
+                "Poder mágico": 0,
+
+            }
+        },
+    ],
+}
+
+print(razas)
